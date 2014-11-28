bdfe1082afecc5f370ec45745cf2d9a4a3b4b5b8
Omar Ahmed <oa12gb@Omars-MacBook-Pro-6.local>
2014-11-28 19:48:11 +0200

    Fixing platform compatibility issues for glut.h

---
+++ Bouncy/Bouncy/PlatformCompatibility.h
@@ -0,0 +1,14 @@
+//
+//  PlatformCompatibility.h
+//  Bouncy
+//
+//  Created by Omar Ahmed on 11/28/14.
+//  Copyright (c) 2014 oa12gb. All rights reserved.
+//
+
+#ifndef Bouncy_PlatformCompatibility_h
+#define Bouncy_PlatformCompatibility_h
+
+#include <GLUT/GLUT.h>
+
+#endif
