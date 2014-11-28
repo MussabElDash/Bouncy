eea6b4fbd207309988f492b011e45bbfd4cf4b82
Omar Ahmed <oa12gb@Omars-MacBook-Pro-6.local>
2014-11-28 19:41:02 +0200

    Added wall constructor

--- Bouncy/Bouncy/Wall.cpp
+++ Bouncy/Bouncy/Wall.cpp
@@ -9,7 +9,20 @@
 #include "Wall.h"
 
 Wall::Wall(POINT startCorner, POINT endCorner, POINT deltaVector){
+    int xSize = (endCorner.X - startCorner.X + deltaVector.X - 1) / deltaVector.X;
+    int ySize = (endCorner.Y - startCorner.Y + deltaVector.Y - 1) / deltaVector.Y;
+    int zSize = (endCorner.Z - startCorner.X + deltaVector.X - 1) / deltaVector.Z;
     
+    wallColors = std::vector<std::vector<std::vector<COLOR>>>(
+        xSize,
+        std::vector<std::vector<COLOR>>(
+            ySize,
+            std::vector<COLOR>(
+                zSize,
+                MAKE_COLOR(rand() % 256, rand() % 256, rand() % 256)
+            )
+        )
+    );
 }
 
 // Draw a wall and keep it in the stack
@@ -20,4 +33,9 @@ void Wall::draw(){
 // If the ball hits this wall, returns the score of this hit, else return 0
 int Wall::getScore(POINT center, int radius){
     return 0;
+}
+
+int Wall::getCellScore(POINT point){
+    COLOR color = wallColors[point.X][point.Y][point.Z];
+    return (color.R - color.G) * color.B;
 }
